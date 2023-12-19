9a39515d55d749ce9a2cf094111ea7184fb2d3b1
João Gabriel <joao.mira@fatec.sp.gov.br>
2023-12-19 13:20:50 -0300

    fix api
    ip fixo da api

--- src/model/index.js
+++ src/model/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default function Connection() {
-    const url = 'http://promo.mfhosting.com.br/';
+    const url = 'http://172.31.41.220:3000';
 	//const url = 'http://localhost:3000/';
 
     const conn = axios.create({
