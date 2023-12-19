8e4df892fedd5dd83f47e95941088ee36e878fa7
Sabha <108093038+fsabha@users.noreply.github.com>
2023-12-19 20:03:04 -0300

    Update index.js
    ajuste no endereço base

--- src/model/index.js
+++ src/model/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default function Connection() {
-    const url = 'http://172.31.41.220:3000/';
+    const url = 'http://ec2-3-144-131-74.us-east-2.compute.amazonaws.com:3000/';
 	//const url = 'http://localhost:3000/';
 
     const conn = axios.create({
